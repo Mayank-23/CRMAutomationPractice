c1baa07cbfa90c25160fa771987e54428e2f5cb9
Mayank-23 <mayank.nigam23@gmail.com>
2023-07-17 20:27:58 +0530

    CRM Automation Practice

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,7 @@
+package utils;
+
+public interface Constants {
+	
+	String url= "https://in.freecrm.com/";
+	
+}
---
+++ src/main/java/utils/SuiteListener.java
@@ -0,0 +1,29 @@
+package utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.internal.annotations.IAnnotationTransformer;
+
+import base.BaseTest;
+
+public class SuiteListener implements ITestListener, IAnnotationTransformer {
+	
+	public void onTestFailure(ITestResult result) {
+	    System.out.println("Checlll");
+		String filename = System.getProperty("user.dir")+File.separator+"screenshots"+File.separator+result.getMethod().getMethodName();
+		File f1 = ((TakesScreenshot)BaseTest.driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(f1, new File(filename+".png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  }
+
+}
